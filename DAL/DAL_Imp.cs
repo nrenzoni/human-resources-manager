ff55d086fb9b37b86f00ffc5e5ab5d6c05bbd15e
Nechemia <nrenzoni1@gmail.com>
2017-01-03 12:12:49 +0200

    generic methods in DAL_Imp

--- DAL/DAL_Imp.cs
+++ DAL/DAL_Imp.cs
@@ -29,7 +29,18 @@ namespace DAL
             else throw new Exception("cannot delete, specilization not found in DS");
         }
 
-        bool update
+        /// act performs action on matching element in list
+        bool updateInList<T>(List<T> list, T element, Action<T> act) where T : class
+        {
+            // need to check if tmp is copy or reference. if value is updated in list or just the local copy
+            var tmp = list.Where(x => x == element).FirstOrDefault();
+            if (tmp != null)
+            {
+                act(tmp); // perform action on found element in list
+                return true;
+            }
+            else throw new Exception("cannot update element, not found in " + nameof(list));
+        }
 
         public bool addSpecilization(Specialization specilization) => 
             addToList(DataSource.specList, specilization); // returns retuned value of addToList
@@ -40,27 +51,18 @@ namespace DAL
         // only minWage and maxWage updated. Cannot update ID and specilization Name
         public bool updateSpecilization(Specialization specilization)
         {
-            // check if copy or reference. if value is updated in list or just the local copy
-            var tmp = DataSource.specList.Where(s => s == specilization).FirstOrDefault();
-            if (tmp != null)
+            return updateInList(DataSource.specList, specilization, s =>
             {
-                tmp.maxWagePerHour = specilization.maxWagePerHour;
-                tmp.minWagePerHour = specilization.minWagePerHour;
-                return true;
-            }
-            else throw new Exception("cannot update specilization, not found in DS");
+                s.maxWagePerHour = specilization.maxWagePerHour;
+                s.minWagePerHour = specilization.minWagePerHour;
+            });
         }
+        
+        public bool addEmployee(Employee employee) =>
+            addToList(DataSource.employeeList, employee);
 
-
-        public bool addEmployee(Employee employee)
-        {
-
-        }
-
-        public bool deleteEmployee(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
+        public bool deleteEmployee(Employee employee) =>
+            deleteFromList(DataSource.employeeList, employee);
 
         public bool updateEmployee(Employee employee)
         {
