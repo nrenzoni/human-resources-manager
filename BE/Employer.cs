1ce1c4c92989932f1ad24eb2674dd7aa9f635469
Nechemia <nrenzoni1@gmail.com>
2017-01-18 20:10:33 +0200

    INotifyPropertyChanged implemented for Employer

--- BE/Employer.cs
+++ BE/Employer.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BE
 {
-    public class Employer
+    public class Employer : INotifyPropertyChanged
     {
         //public Employer(uint ID, string companyname, uint phoneNum, CivicAddress addr, SpecializationName specName, DateTime estabDate, string firstname="", string lastname="", bool isPrivate=false)
         //{
@@ -27,18 +28,102 @@ namespace BE
         public uint ID { get; set; }
 
         // if privatePerson
-        public string firstName { get; set; }
-        public string lastName{ get; set; }
+        string _firstName;
+        public string firstName {
+            get { return _firstName; }
+            set
+            {
+                if(_firstName != value)
+                {
+                    _firstName = value;
+                    NotifyPropertyChanged("firstName");
+                }
+            }
+        }
+
+        string _lastName;
+        public string lastName{
+            get {return _lastName; }
+            set
+            {
+                if(_lastName != value)
+                {
+                    _lastName = value;
+                    NotifyPropertyChanged("lastName");
+                }
+            }
+        }
 
         // optional if private person
-        public string companyName { get; set; }
+        string _companyName;
+        public string companyName
+        {
+            get { return _companyName; }
+            set
+            {
+                if(_companyName != value)
+                {
+                    _companyName = value;
+                    NotifyPropertyChanged("companyName");
+                }
+            }
+        }
 
-        public uint phoneNumber { get; set; }
-        public CivicAddress address { get; set; }
+        uint _phoneNumber;
+        public uint phoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                if (_phoneNumber != value)
+                {
+                    _phoneNumber = value;
+                    NotifyPropertyChanged("phoneNumber");
+                }
+            }
+        }
 
-        public SpecializationName specializationName { get; set; } // enum
+        CivicAddress _address;
+        public CivicAddress address
+        {
+            get { return _address; }
+            set
+            {
+                if (_address != value)
+                {
+                    _address = value;
+                    NotifyPropertyChanged("address");
+                }
+            }
+        }
 
-        public DateTime establishmentDate { get; set; }
+        SpecializationName _specializationName;  // enum
+        public SpecializationName specializationName
+        {
+            get { return _specializationName; }
+            set
+            {
+                if (_specializationName != value)
+                {
+                    _specializationName = value;
+                    NotifyPropertyChanged("specializationName");
+                }
+            }
+        }
+
+        DateTime _establishmentDate;
+        public DateTime establishmentDate
+        {
+            get { return _establishmentDate; }
+            set
+            {
+                if (_establishmentDate != value)
+                {
+                    _establishmentDate = value;
+                    NotifyPropertyChanged("establishmentDate");
+                }
+            }
+        }
 
         public override string ToString()
         {
@@ -49,6 +134,14 @@ namespace BE
                 ((string.IsNullOrEmpty(companyName)) ? "" : " Company Name: " + companyName) +
                 " Phone Number: " + phoneNumber +
                 ", " + address;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
 
         public static bool operator ==(Employer e1, Employer e2)
