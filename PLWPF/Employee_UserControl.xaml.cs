f3ef10d36bb718c8e33086aeaefbd8c1b6a2aa43
Almog Mishraki <mog7599@gmail.com>
2017-02-02 09:32:45 +0200

    Finish employee addClick button

--- PLWPF/Employee_UserControl.xaml.cs
+++ PLWPF/Employee_UserControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Reflection;
 
 namespace PLWPF
 {
@@ -60,58 +61,18 @@ namespace PLWPF
         {
             try
             {
-                List<object> checkValue = new List<object> ();
-
-                var employeID = ComEmplyeeID.SelectedValue;
-                checkValue.Add(employeID);
-
-                var employeLName = txtLastName.Text;
-                checkValue.Add(employeLName);
-
-                var employeFName = txtFirstName.Text;
-                checkValue.Add(employeFName);
-
-                var employePhoneNum = int.Parse(txtPhoneNumber.Text);
-                checkValue.Add(employePhoneNum);
-
-                var employeAddress = txtEmplyeeAdd.Text;
-                checkValue.Add(employeAddress);
-
-                var employeCity = ComEmplyeeCity.SelectedValue;
-                checkValue.Add(employeCity);
-
-                var employeExpYears = txtExperiance.Text;
-                checkValue.Add(employeExpYears);
-
-                var employeEdu = ComEmplyeeEduc.SelectedItem;
-                checkValue.Add(employeEdu);
-
-                var employeSpec = ComEmployeSpec.SelectedItem;
-                checkValue.Add(employeSpec);
-
-                //NEED TO IMPLEMENT THE CHECKS IF THE BANK DETAILS ARE EMPTY, 
-                //    WHEN FINISH TO CONNECT THE XML!~!~!
-
-                foreach (var item in checkValue)
+                foreach (var property in UIEmployee.GetType().GetProperties())
                 {
-                    if (item==null)
-                        throw new Exception("please fill out all fields");
+                    if (property.Name != "recommendationNotes")
+                    {
+                        if (property.GetGetMethod() != null)
+                        {
+                            if (property.GetValue(UIEmployee) == null)
+                                throw new Exception("please fill out all fields");
+                        }
+                    }
                 }
                 
-                UIEmployee.ID = (uint)employeID;
-                UIEmployee.lastName = employeLName;
-                UIEmployee.firstName = employeFName;
-                UIEmployee.phoneNumber = (uint)employePhoneNum;
-                UIEmployee.address.Address = employeAddress;
-                UIEmployee.address.City =(string)employeCity;
-                UIEmployee.yearsOfExperience = uint.Parse(employeExpYears);
-               // UIEmployee.education = Enum. employeEdu;
-                //UIEmployee.specializationID = employeSpec;
-
-
-
-
-
                 BE.Employee addEmploye = new BE.Employee();
                 Globals.CopyObject(UIEmployee, addEmploye);
                 Bl_Object.addEmployee(addEmploye);
